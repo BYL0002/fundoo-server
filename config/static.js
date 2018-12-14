8aec25dfdfcd8ae247b6a3216d202509c2e6c524
Yash <yash.sharma0002@gmail.com>
2018-12-14 13:31:34 +0530

    status 01

--- config/static.js
+++ config/static.js
@@ -1,7 +1,7 @@
 module.exports = {
     issuer: 'bridgelabz',
     audience: 'http://mysoftcorp.in',
-    expiresIn: '1h',
+    expiresIn: '1d',
     service: 'gmail',
     subject: 'http://mysoftcorp.in',
     url_setpassword : 'http://localhost:3000/setpassword/'

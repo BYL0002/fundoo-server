e539654c9c7ebc4303fbf22bc6a5d4f60c57895a
Yash <yash.sharma0002@gmail.com>
2019-01-08 19:19:18 +0530

    collab 03

--- services/CollabService.js
+++ services/CollabService.js
@@ -31,7 +31,7 @@ exports.collabAddService = function (req, callback) {
  */
 exports.collabDisplayService = function (req, callback) {
 
-    collabmodel. .labelDisplayModel(req, (err, data) => {
+    collabmodel.labelDisplayModel(req, (err, data) => {
         if (err) {
             return callback(err);
         }
@@ -85,110 +85,3 @@ exports.getCollabDetails = (callback) => {
         }
     })
 }
-
-
-// ------------------------------------------------------
-
-// /**
-//  * @param {object} data
-//  * @param {callback function} callback
-//  */
-// exports.getNotes = (userIDRes, callback) => {
-//     // user.findUserID(data, (err, userIDRes) => {
-//     //     if (err) {
-//     //         callback(err);
-//     //     }
-//     //     else {
-//             var finalResult = [];
-//             note.getNotes(userIDRes, (err, result) => {
-//                 if (err) {
-//                     callback(err);
-//                 } else {
-//                     user.findByUserId(userIDRes, (errorUser, resultUser) => {
-//                         if (errorUser) {
-//                             callback(errorUser);
-//                         } else {
-//                             const noteOwner ={
-//                                 firstname:resultUser.firstname,
-//                                 lastname:resultUser.lastname,
-//                                 username:resultUser.username,
-//                                 _id:resultUser._id
-//                             }
-//                             for (var i = 0; i < result.length; i++) {
-//                                 var userNote = {
-//                                     note: result[i],
-//                                     owner: noteOwner,
-//                                     collab: []
-//                                 }
-//                                 finalResult.push(userNote);
-//                             }
-//                             collabService.getCollabOwnerUserId(userIDRes, (errorCollab, resultOwnerCollab) => {
-//                                 if (errorCollab) {
-//                                     callback(errorCollab);
-//                                 } else {
-//                                     for (var i = 0; i < finalResult.length; i++) {
-//                                         for (var j = 0; j < resultOwnerCollab.length; j++) {
-                                            
-//                                             if (finalResult[i].note._id.equals(resultOwnerCollab[j].noteID)) {                                               
-//                                                 finalResult[i].collab.push(resultOwnerCollab[j].collabUserID)
-//                                             }
-//                                         }
-//                                     }
-//                                 }
-//                             })
-//                             collabService.getCollabNotesUserId(userIDRes, (errorCollab, resultCollab) => {
-//                                 if (errorCollab) {
-//                                     callback(errorCollab);
-//                                 } else {
-//                                     var operations = [];
-//                                     for (var i = 0; i < resultCollab.length; i++) {
-//                                         operations.push((function (collabData) {
-
-//                                             return function (callback) {
-
-//                                                 collabService.getDataByNoteId(collabData.noteID, (errorNote, resultNote) => {
-//                                                     console.log("123 : ", resultNote);
-
-//                                                     if (errorNote) {
-//                                                         callback(errorNote)
-//                                                     } else {
-//                                                         var collabUserArray = [];
-//                                                         for (var i = 0; i < resultNote.length; i++) {
-//                                                             collabUserArray.push(resultNote[i].collabUserID)
-//                                                         }
-//                                                         var collabNote = {
-//                                                             note: resultNote[0].noteID,
-//                                                             owner: resultNote[0].userID,
-//                                                             collab: collabUserArray
-//                                                         }
-//                                                         finalResult.push(collabNote);
-//                                                         callback(null, collabNote)
-
-//                                                     }
-//                                                 })
-//                                             }
-
-//                                         })(resultCollab[i]))
-//                                     }
-//                                     async.series(operations, (errorAsync, resultAsync) => {
-//                                         console.log(resultAsync);
-
-//                                         if (errorAsync) {
-//                                             callback(errorAsync);
-//                                         } else {
-//                                             console.log("final result ", finalResult);
-
-//                                             callback(null, finalResult)
-//                                         }
-//                                     })
-//                                 }
-//                             })
-//                         }
-//                     })
-//                 }
-//             })
-//         // }
-//     // })
-// }
-
-
